dff80dd961f6ab396350d9d3a5d69e1d2e0688c9
Chaoui-Wissal <wissal.chaoui.16@gmail.com>
2020-07-13 16:02:14 +0100

    app project

---
+++ Config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAoILBzDjIhyFQzSBlCRxsK616HyjYo7Ls",
+    authDomain: "movies-215dc.firebaseapp.com",
+    databaseURL: "https://movies-215dc.firebaseio.com",
+    projectId: "movies-215dc",
+    storageBucket: "movies-215dc.appspot.com",
+    messagingSenderId: "206158759288",
+    appId: "1:206158759288:web:fe952475c87f6103dc348d",
+    measurementId: "G-29Y54RB92J"
+  };
